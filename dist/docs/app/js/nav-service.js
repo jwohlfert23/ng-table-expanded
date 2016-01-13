afbe8a0042ef84470bb4e1910d73d364b0154bf1
Jack Wohlfert <jack.wohlfert@gmail.com>
2016-01-13 16:11:31 -0500

    upgrade to new bootstrap

---
+++ dist/docs/app/js/nav-service.js
@@ -0,0 +1,86 @@
+module.exports = {
+    name:'NAVSERVICE',
+    data:[
+  {
+    "title": "API Docucmentation",
+    "name": "api",
+    "url": "/docs/api",
+    "items": [
+      {
+        "type": "module",
+        "items": [
+          {
+            "name": "ngTable",
+            "url": "partials/api/ngTable/index.html"
+          }
+        ]
+      },
+      {
+        "type": "service",
+        "items": [
+          {
+            "name": "ngTableEventsChannel",
+            "url": "partials/api/./service/ngTableEventsChannel.html"
+          },
+          {
+            "name": "ngTableDefaultGetData",
+            "url": "partials/api/./service/ngTableDefaultGetData.html"
+          },
+          {
+            "name": "ngTableColumn",
+            "url": "partials/api/ngTable/service/ngTableColumn.html"
+          },
+          {
+            "name": "NgTableParams",
+            "url": "partials/api/ngTable/service/NgTableParams.html"
+          },
+          {
+            "name": "ngTableParams",
+            "url": "partials/api/./service/ngTableParams.html"
+          }
+        ]
+      },
+      {
+        "type": "directive",
+        "items": [
+          {
+            "name": "ngTable",
+            "url": "partials/api/ngTable/directive/ngTable.html"
+          },
+          {
+            "name": "ngTableDynamic",
+            "url": "partials/api/ngTable/directive/ngTableDynamic.html"
+          },
+          {
+            "name": "ngTablePagination",
+            "url": "partials/api/ngTable/directive/ngTablePagination.html"
+          }
+        ]
+      },
+      {
+        "type": "object",
+        "items": [
+          {
+            "name": "ngTableDefaultParams",
+            "url": "partials/api/ngTable/object/ngTableDefaultParams.html"
+          },
+          {
+            "name": "settings",
+            "url": "partials/api/ngTable/object/settings.html"
+          },
+          {
+            "name": "ngTableController",
+            "url": "partials/api/./object/ngTableController.html"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "title": "Examples",
+    "name": "examples",
+    "url": "/docs/examples",
+    "items": []
+  }
+]
+};
